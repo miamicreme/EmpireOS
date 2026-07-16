6f3692c204ba5e0e477ad8296020ca4485f0b40b
Miami Creme Labs <miamicreme@gmail.com>
2026-07-16 17:15:19 -0400

    Add Empire voice page

---
+++ src/app/empire/page.tsx
@@ -0,0 +1,9 @@
+import { EmpireVoiceConsole } from '@/components/empire/EmpireVoiceConsole';
+
+export default function EmpirePage() {
+  return (
+    <main className="min-h-screen bg-surface-0 px-4 py-20 sm:px-6 lg:px-10 lg:py-10">
+      <EmpireVoiceConsole />
+    </main>
+  );
+}
